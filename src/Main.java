82f562811c718bba214d49a163a7bc96ce806a43
solaimanope <solaimancsebuet16@gmail.com>
2020-03-04 16:50:27 +0600

    check commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+    public static final int DIM = 4;
     public static void main(String[] args) {
+        for (int i = 0; i < DIM; i++) {
+            for (int j = 0; j < DIM; j++) {
 
+            }
+        }
     }
 }
